3cec9d17eea6256a9097190bc4c0920515555a55
marcelo costa da mata <mcosta.matta@gmail.com>
2022-05-29 18:47:04 -0300

    Desenvolvimento e teste

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Criação do repositório
 Configurações iniciais e criação do .gitignore e app.py
 Desenvimento do arquivo app e aplicação
+Teste no arquivo requests apresenta erro na resposta
--- app.py
+++ app.py
@@ -20,3 +20,24 @@ async def opSoma(valor_1: int, valor_2: int, oper: str):
     if oper == 'sm'.lower() or '+':
         soma = (valor_1 + valor_2)
         return soma
+
+
+@app.post("/subtracao")
+async def opSubtracao(valor_1: int, valor_2: int, oper: str):
+    if oper == 'sb'.lower() or '-':
+        subtracao = (valor_1 - valor_2)
+        return subtracao
+
+
+@app.post("/multiplicacao")
+async def opMultp(valor_1: int, valor_2: int, oper: str):
+    if oper == 'mt'.lower() or '*':
+        multiplicacao = (valor_1 * valor_2)
+        return multiplicacao
+
+
+@app.post("/divisao")
+async def opDivisao(valor_1: int, valor_2: int, oper: str):
+    if oper == 'dv'.lower() or '/':
+        divisao = (valor_1 / valor_2)
+        return divisao
